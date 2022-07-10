3018286b40b6a3e8287abbf9e19c96a8184223b3
rahul <rahul@gamil.com>
2022-07-10 16:09:10 +0530

    moving avarage stategy

--- v1 backtesting.py
+++ v1 backtesting.py
@@ -2,11 +2,6 @@
 import backtrader as bt
 import pandas as pd
 import datetime
-from matplotlib.dates import (HOURS_PER_DAY, MIN_PER_HOUR, SEC_PER_MIN,
-                              MONTHS_PER_YEAR, DAYS_PER_WEEK,
-                              SEC_PER_HOUR, SEC_PER_DAY,
-                              num2date, rrulewrapper, YearLocator,
-                              MicrosecondLocator)
 
 import backtesting
 import matplotlib
